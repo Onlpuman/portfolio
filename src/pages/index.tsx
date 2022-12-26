aee738cc6860b0373bb44fc02da09ec9731d4eaf
onlyhuman <onlyhuman@mail.ru>
2022-12-27 01:53:52 +0200

    Projects page. Added a Project page and a style for it.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,43 @@
-import Head from 'next/head';
+import { GetServerSideProps, NextPage } from 'next';
 
-import type { NextPage } from 'next';
+import { IProjects } from '../../models';
+import Header from '../components/Header/Header';
 
-const Home: NextPage = () => {
+import styles from './Projects.module.scss';
+
+type projectProps = {
+	projects: [IProjects],
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+	try {
+		const res = await fetch(`${process.env.API_HOST}/projects/`);
+		const data:IProjects[] = await res.json();
+		
+		return {
+			props: { projects: data },
+		};
+	} catch (error) {
+		return { notFound: true };
+	}
+};
+
+const Projects: NextPage<projectProps> = ({ projects }) => {
+	if (!projects) return null;
+	
 	return (
-		<div>
-			<Head>
-				
-			</Head>
-
-			<main>
-			
-			</main>
-
-			<footer>
-			
-			</footer>
-		</div>
+		<>
+			<Header />
+			<section className={styles.section}>
+				<div className={styles.container}>
+					<h2 className={styles.title}>Projects</h2>
+					<ul className={styles.projects}>
+					
+					</ul>
+				</div>
+			</section>
+		</>
 	);
 };
 
-export default Home;
+export default Projects;
