e991fb5e6b7a27965bece546b8c907fa115d1e08
onlyhuman <onlyhuman@mail.ru>
2022-12-26 20:56:28 +0200

    Added:  - "App" wrapper in context and "Layout" component;  - receiving a cookie with a dark theme value from the server.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,42 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { AppProps } from 'next/app';
+import { NextPageContext } from 'next';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import { ThemeProvider } from '../context/ThemeContext';
+import Layout from '../components/Layout/Layout';
+import '../styles/reset.css';
+import '../styles/global.scss';
+
+type serverProps = {
+	ctx: NextPageContext;
 }
 
-export default MyApp
+const MyApp = ({ Component, pageProps }: AppProps) => {
+	const { pathFromServer, isDarkMode } = pageProps;
+	
+	return (
+		<ThemeProvider isDarkMode={isDarkMode} pathFromServer={pathFromServer}>
+			<Layout>
+				<Component {...pageProps} />
+			</Layout>
+		</ThemeProvider>
+	);
+};
+
+MyApp.getInitialProps = async ({ ctx }: serverProps) => {
+	const cookie = ctx?.req?.headers?.cookie?.match(/isDarkMode=(\w+);?/)?.[1];
+	const isDarkMode =
+		cookie === undefined
+			? false
+			: JSON.parse(cookie);
+	
+	const pathFromServer = ctx.pathname;
+	
+	return {
+		pageProps: {
+			pathFromServer,
+			isDarkMode,
+		},
+	};
+};
+
+export default MyApp;
