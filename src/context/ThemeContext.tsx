0afa312a878f1bdaa9219f917fbae454366d44d7
onlyhuman <onlyhuman@mail.ru>
2022-12-26 21:54:01 +0200

    ThemeContext component. Implementation of the dark theme switching logic.

--- src/context/ThemeContext.tsx
+++ src/context/ThemeContext.tsx
@@ -1,24 +1,60 @@
-import React, { createContext, FC, ReactNode, useState } from 'react';
+import React, { createContext, FC, ReactNode, useEffect, useLayoutEffect, useState } from 'react';
+import Cookies from 'universal-cookie';
+
+import { isServer } from '../helpers/isServer';
 
 type ThemeProviderProps = {
 	children: ReactNode,
 	isDarkMode: boolean,
+	pathFromServer: string
 }
 
-type ThemeType = {
+type ValueType = {
 	isDarkTheme: boolean;
 	setIsDarkTheme: React.Dispatch<React.SetStateAction<boolean>>
 }
 
-export const ThemeContext = createContext<ThemeType>({} as ThemeType);
+export const ThemeContext = createContext<ValueType>({} as ValueType);
 
 const ThemeProvider: FC<ThemeProviderProps> = ({ children, isDarkMode }) => {
 	const [isDarkTheme, setIsDarkTheme] = useState<boolean>(isDarkMode);
 	
-	const value: ThemeType = {
+	const value: ValueType = {
 		isDarkTheme,
 		setIsDarkTheme,
 	};
+	
+	const cookies = new Cookies();
+	
+	const sendCookieTheme = (value: boolean) => {
+		cookies.set('isDarkMode', value, {
+			path: '/',
+		});
+	};
+	
+	useLayoutEffect(() => {
+		if (!isServer()) {
+			const mediaQueryList = window.matchMedia('(prefers-color-scheme: dark)');
+			
+			const funcListener = (e: MediaQueryListEvent) => {
+				const isUserDarkTheme = e.matches;
+				isUserDarkTheme
+					? sendCookieTheme(true)
+					: sendCookieTheme(false);
+				
+				setIsDarkTheme(isUserDarkTheme);
+			};
+			
+			mediaQueryList.addEventListener('change', funcListener);
+		}
+		//return () => .removeEventListener('change', funcListener);
+	}, []);
+	
+	useEffect(() => {
+		if (!isServer()) {
+			sendCookieTheme(isDarkTheme);
+		}
+	}, [isDarkTheme]);
 	
 	return (
 		<ThemeContext.Provider value={value}>
