0b5679f1380690f4b619fae1a16ff73f2ffcadeb
onlyhuman <onlyhuman@mail.ru>
2022-12-26 22:13:29 +0200

    FooterLinks, Footer component. Added FooterLinks and styles for it.

--- src/components/Layout/Footer/Footer.tsx
+++ src/components/Layout/Footer/Footer.tsx
@@ -1,11 +1,13 @@
 import { FC } from 'react';
 
+import FooterLinks from './FooterLinks/FooterLinks';
 import styles from './Footer.module.scss';
 
 const Footer:FC = () => (
 	<footer className={styles.footer}>
 		<div className={styles.container}>
 			<div className={styles.footer__wrapper}>
+				<FooterLinks />
 				<div className={styles.copyright}>
 					<p>2022</p>
 				</div>
---
+++ src/components/Layout/Footer/FooterLinks/FooterLinks.tsx
@@ -0,0 +1,25 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import { FC } from 'react';
+
+import { footerLinks } from '../../layoutData/layoutData';
+
+import styles from './FooterLinks.module.scss';
+
+const FooterLinks:FC = () => (
+	<ul className={styles.social}>
+		{footerLinks.map(({
+			id,
+			src,
+			path,
+		}) => (
+			<li className={styles.item} key={id}>
+				<Link href={path}>
+					<Image src={src} alt="Links" width={38} height={38}/>
+				</Link>
+			</li>
+		))}
+	</ul>
+);
+
+export default FooterLinks;
