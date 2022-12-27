21a01a6b8d3ca92c22bf5180a250fc604d73c2cf
onlyhuman <onlyhuman@mail.ru>
2022-12-27 02:57:29 +0200

    layoutData -> data. Changing the file name and location. Adding data for the Skills component.

--- data/data.ts
+++ data/data.ts
@@ -1,4 +1,4 @@
-import { IFooterLink, INavLink } from '../../../../models';
+import { IFooterLink, INavLink } from '../models';
 
 export const navLinks:INavLink[] = [
 	{ id: 1, title: 'Projects', path: '/' },
@@ -10,4 +10,20 @@ export const footerLinks:IFooterLink[] = [
 	{ id: 1, src: '/icons/gitHub.svg', path: 'https://github.com/Onlpuman' },
 	{ id: 2, src: '/icons/linkedIn.svg', path: 'https://www.linkedin.com/in/maksim-p-297804237' },
 	{ id: 3, src: '/icons/instagram.svg', path: 'https://www.instagram.com/onlypuman/' },
+];
+
+export const skills: string[] = [
+	'JavaScript',
+	'TypeScript',
+	'ReactJS',
+	'NextJS',
+	'Redux',
+	'HTML',
+	'CSS',
+	'SCSS',
+	'NPM',
+	'Yarn',
+	'BootStrap',
+	'MaterialUI',
+	'StyledComponents',
 ];
