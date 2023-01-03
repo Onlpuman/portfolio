56eecf36dd94bc5c1ef7909f1bacfa65b42c0309
onlyhuman <onlyhuman@mail.ru>
2023-01-03 19:21:53 +0200

    nextConfig update

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@
 const nextConfig = {
 	reactStrictMode: true,
 	swcMinify: true,
+	i18n: {
+		locales: ['en'],
+		defaultLocale: 'en',
+	},
 	images: {
 		deviceSizes: [375, 390, 412, 600, 640, 750, 828, 1080, 1200, 1280, 1440, 1600, 1920, 2048, 3840],
 	},
