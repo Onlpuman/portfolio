d03bb2bf92716402327c7691a2dab23e9221ccd7
onlyhuman <onlyhuman@mail.ru>
2023-01-03 05:08:28 +0200

    nextConfig update

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
 	reactStrictMode: true,
 	swcMinify: true,
 	images: {
-		deviceSizes: [260, 320, 390, 412, 600, 640, 750, 828, 1080, 1200, 1280, 1440, 1600, 1920, 2048, 3840],
+		deviceSizes: [375, 390, 412, 600, 640, 750, 828, 1080, 1200, 1280, 1440, 1600, 1920, 2048, 3840],
 	},
 	experimental: {
 		scrollRestoration: true,
